3271f064374173e44b5c500c2e7a18cf68736b24
Oopskill <avala4e@gmail.com>
2022-06-11 11:09:56 +0300

    Update run.py

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ from modules.password_generator.module import generate_password_module
 from modules.what_eat.module import chose_food_module
 from modules.hackernews.module import hackernews_module
 from modules.vacancies.module import random_vacancy_module
+from modules.joke_generator.module import joke_generator_module
 #from modules.film_generator.module import generate_film_module
 
 config = configparser.ConfigParser()
@@ -24,6 +25,7 @@ bot.register(hackernews_module, ['hackernews',"news","новости","каки�
 bot.register(random_vacancy_module, ['Вакансия','Вакансии','Вакансии Курск', 'вакансия'], "А если напишешь мне 'вакансия' - я постараюсь найти вакансию твоей мечты в Норбит!")
 bot.register(generate_news_module, ['Новость','новость','новости', 'Новости'], "А если напишешь мне 'новость' - я расскажу тебе последнюю новость в России!")
 #bot.register(generate_film_module, ['Фильм','фильм','Кино', 'Фильмы'], "А если напишешь мне 'фильм' - я предложу тебе фильм!")
+bot.register(joke_generator_module, ['шутка','шутки','пошути', 'хочу посмеяться'], "А если напишешь мне 'шутка' - я расскажу тебе шутку!")
 
 bot.run()
 
